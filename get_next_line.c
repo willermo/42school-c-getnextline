15cbee19bc99d3847395a6a6ade3edf4da16d407
davide.oriani <willermo@gmail.com>
2023-04-06 00:53:31 +0200

    try

--- get_next_line.c
+++ get_next_line.c
@@ -6,37 +6,57 @@
 /*   By: doriani <doriani@student.42roma.it>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 16:50:22 by doriani           #+#    #+#             */
-/*   Updated: 2023/04/04 00:25:59 by doriani          ###   ########.fr       */
+/*   Updated: 2023/04/06 00:49:11 by doriani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static void set_line(char **line, char *buffer)
+{
+	int 	len;
+	char	*runner;
+	*line = malloc((ft_strchr(buffer, '\n') - buffer + 1) * sizeof(char));
+	if (!*line)
+		return ;
+	len = ft_strlen(buffer);
+	runner = buffer;
+	while (*buffer != '\n' && *buffer != '\0')
+		*line++ = *buffer++;
+	*line = '\0';
+	while (*buffer != '\0')
+	{
+		*runner++ = *buffer++;
+		len--;
+	}
+	while(len--)
+		*runner++ = '\0';
+}
+
 char	*get_next_line(t_fd fd)
 {
-	char						*line;
-	static char					buffer[BUFFER_SIZE];
-	static int					bytes_read;
-	static size_t				bytes_remaining;
-	size_t						i;
+	char		*line;
+	char		*read_buffer;
+	static char	*line_buffer;
+	int			bytes_read;
+	size_t		dim;
 
-	line = NULL;
-	if (bytes_read == 0)
-		bytes_remaining = read(fd, buffer, BUFFER_SIZE);
-	i = 0;
-	while (fd > 0 && BUFFER_SIZE > 0 && bytes_remaining)
+	if (fd < 0 || fd >= 4096 || fd == 1 || fd == 2 || BUFFER_SIZE <= 0)
+		return(NULL);
+	read_buffer = ft_calloc(BUFFER_SIZE, sizeof(char));
+	if (!read_buffer)
+		return (NULL);
+	dim = 0;
+	bytes_read = 0;
+	while (!ft_strchr(line_buffer, '\n') && bytes_read >= 0)
 	{
-		if (i % BUFFER_SIZE == 0)
-			line = expand_line_buffer(line, bytes_read / BUFFER_SIZE + 1);
-		line[i++] = buffer[bytes_read++ % BUFFER_SIZE];
-		if (fd == 0 && line[i - 1] == '\n')
-			bytes_read = 0;
-		else if (--bytes_remaining == 0)
-			bytes_remaining = read(fd, buffer, BUFFER_SIZE);
-		if (line[i - 1] == '\n')
-			break ;
+		line_buffer = expand_line_buffer(line_buffer, dim++);
+		bytes_read = read(fd, read_buffer, BUFFER_SIZE);
+		ft_memset(read_buffer + bytes_read, 0, BUFFER_SIZE - bytes_read);
+		ft_strlcat(line_buffer, read_buffer, BUFFER_SIZE * dim);
+		ft_memset(read_buffer, 0, BUFFER_SIZE);
 	}
-	if (!line)
-		bytes_read = 0;
+	set_line(&line, line_buffer);
+	free(read_buffer);
 	return (line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: doriani <doriani@student.42roma.it>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 16:50:31 by doriani           #+#    #+#             */
-/*   Updated: 2023/04/03 01:09:38 by doriani          ###   ########.fr       */
+/*   Updated: 2023/04/06 00:24:28 by doriani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*expand_line_buffer(char *line, size_t dim)
 	new_line = (char *) ft_calloc(sizeof(char), dim * BUFFER_SIZE + 1);
 	if (!line)
 		return (new_line);
-	ft_memcpy(new_line, line, (dim - 1) * BUFFER_SIZE);
+	ft_strlcat(new_line, line, dim * BUFFER_SIZE + 1);
 	free(line);
 	return (new_line);
 }
@@ -38,6 +38,47 @@ void	*ft_calloc(size_t count, size_t size)
 	return (ptr);
 }
 
+char	*ft_strchr(const char *s, int c)
+{
+	char	*runner;
+
+	runner = (char *) s;
+	while (*runner)
+	{
+		if (*runner == c)
+			return (runner);
+		runner++;
+	}
+	if (*runner == c)
+		return (runner);
+	return (NULL);
+}
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	char	*p;
+
+	p = dest;
+	while (size != 0 && *p != '\0')
+	{
+		p++;
+		size--;
+	}
+	if (size != 0)
+	{
+		while (--size != 0)
+		{
+			*p = *src;
+			if (*p == '\0')
+				return (p - dest);
+			p++;
+			src++;
+		}
+		*p = '\0';
+	}
+	return ((p - dest) + ft_strlen(src));
+}
+
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t			i;
@@ -53,4 +94,16 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 		i++;
 	}
 	return (dest);
+}
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*runner;
+
+	runner = (unsigned char *) s;
+	i = 0;
+	while (i < n)
+		runner[i++] = c;
+	return (s);
 }
